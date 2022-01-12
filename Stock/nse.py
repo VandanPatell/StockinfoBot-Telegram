dc99f13de50c819012c77a4dc8a8855af494a329
Vandan Patel <59683751+VandanPatell@users.noreply.github.com>
2022-01-12 19:50:07 +0530

    Updates made
    Made necessary updates to some methods and also introduced some new methods.

--- Stock/nse.py
+++ Stock/nse.py
@@ -8,7 +8,7 @@ class NseData:
         self.nse = Nse()
 
     @classmethod
-    def is_valid(cls,stock_code: str) -> bool:
+    def is_valid(cls, stock_code: str) -> bool:
         return cls.nse.is_valid_code(stock_code)
 
     @classmethod
@@ -17,9 +17,17 @@ class NseData:
 
     @classmethod
     def get_all_details(cls, stock_code: str) -> str:
-        raw = cls.nse.get_quote(stock_code)
-        output = f"*Company name*: {raw['companyName']}\n *Stock code*: {raw['symbol']}\n *isinCode*: {raw['isinCode']}\n \N{money bag} *Current price*: {raw['lastPrice']}\n \u2b06 *dayHigh*: {raw['dayHigh']}\n \u2b07 *dayLow*: {raw['dayLow']}\n \N{Chart with Upwards Trend} *52 week high*: {raw['high52']}\n \N{Chart with Downwards Trend} *52 week low*: {raw['low52']}\n "
+        ''' Get the complete details of the stock based on scrip code on NSE '''
+        if NseData.is_valid(stock_code):
+            raw = cls.nse.get_quote(stock_code)
+            output = f"*Company name*: {raw['companyName']}\n *Stock code*: {raw['symbol']}\n *isinCode*: {raw['isinCode']}\n \N{money bag} *Current price*: {raw['lastPrice']}\n \u2b06 *dayHigh*: {raw['dayHigh']}\n \u2b07 *dayLow*: {raw['dayLow']}\n \N{Chart with Upwards Trend} *52 week high*: {raw['high52']}\n \N{Chart with Downwards Trend} *52 week low*: {raw['low52']}\n "
+        else:
+            output = f"Invalid stock code: {stock_code}"
         return output
+
+    def chooseStockName(self, company_name_search: str) -> str:
+        ''' Search the company name on NSE and return the scrip code '''
+        return self.nse.get_code_by_name(company_name_search)
 
     def get_stock_data(self, stock_code: str) -> dict:
         return self.nse.get_quote(stock_code)
@@ -30,13 +38,22 @@ class NseData:
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-
     @classmethod
     def search_company(cls, company_name: str) -> list:
+        ''' Search for a company name on NSE using company name'''
         raw = cls.nse.get_stock_codes()
         lis = list()
         for i in raw:
             if company_name.lower() in raw[i].lower():
-                lis.append(i + " - " + raw[i] + " -\t Rs. " +
-                           str(NseData.get_stock_price(i)))
+                lis.append(raw[i])
         return lis
+
+    @classmethod
+    def get_details_by_name(cls, company_name: str) -> str:
+        ''' Get the complete details of the stock based on company name on NSE '''
+        raw = cls.nse.get_stock_codes()
+        for i in raw:
+            if company_name.lower() in str(raw[i]).lower():
+                # print(str(raw[i]).lower() + " - " + i)
+                return cls.get_all_details(i)
+        return "No such company"
