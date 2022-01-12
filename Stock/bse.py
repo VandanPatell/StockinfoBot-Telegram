f0e1fac75cbdc9f24d7fea2b60c3fb7c3a561767
Vandan Patel <59683751+VandanPatell@users.noreply.github.com>
2022-01-12 19:49:09 +0530

    Fixed some issues
    Fixed issues like taking huge amount of time to create the object of bse class. as init method contained the update function to update the required json files in directory.
    Some of the unused removed and the required ones added.

--- Stock/bse.py
+++ Stock/bse.py
@@ -1,6 +1,6 @@
 import json
 from bsedata.bse import BSE
-# 
+
 
 class bse:
 
@@ -11,68 +11,33 @@ class bse:
         This is bse class that will fetch and return various realtime information from BSE (Bombay Stock Exchange).
         Author : Vandan Patel
         """
-        self.bseObj.updateScripCodes()
-        print("bse Class created")
-
-    def getcripcodes(self):
-        return self.bseObj.getScripCodes()
+        # bseObj.updateScripCodes()
+        # print("bse Class created")
 
     @classmethod
-    def check_scripCode(cls, scripCode: str) -> bool:
-        """
-        This method will check whether the the given scrip code exists or not.
-
-        Args:
-            scripCode (int): the scrip code of the stock for eg: 5128965 
-
-        Returns:
-            bool: Will return the boolean value.
-        """
-        if cls.bseObj.verifyScripCode(scripCode) != None:
-            return True
-
-        return False
-
-    @classmethod
-    def get_company_name(cls, scripCode: str) -> str:
-
-        if bse.check_scripCode(scripCode):
-            return cls.bseObj.verifyScripCode(scripCode)
-        else:
-            return "Invalid ScripCode"
-
-    @classmethod
-    def get_scrip_code(cls, company_alias: str) -> str:
+    def search_companies_by_name(cls, companyName) -> str:
         raw = cls.bseObj.getScripCodes()
-        found = False
-        for i in raw:
-            if company_alias.lower() in raw[i].lower():
-                found = True
-                return str(i)
-
-        if not found:
-            return "No Company found."
-
-    @classmethod
-    def get_stock_price(cls, scripCode: str = None, company_name: str = None) -> int:
-        if scripCode == None and company_name == None:
-            return None
-        elif scripCode == None:
-            raw = cls.bseObj.getScripCodes()
-            for i in raw:
-                if company_name.lower() in raw[i].lower():
-                    json_obj = cls.bseObj.getQuote(i)
-                    return json_obj['currentValue']
-        elif company_name == None:
-            a = cls.bseObj.getQuote(scripCode=scripCode)
-            return a['currentValue']
-
-    @classmethod
-    def searchCompany(cls, companyName) -> str:
-        raw = cls.bseObj.getScripCodes()
-        companies = str()
+        list = []
         for i in raw:
             if companyName.lower() in raw[i].lower():
-                companies = companies + "\n" + raw[i]
+                list.append(raw[i])
+        return list
 
-        return companies
+    @classmethod
+    def get_details_by_name(cls, companyName) -> str:
+        raw = cls.bseObj.getScripCodes()
+        for i in raw:
+            if companyName.lower() == raw[i].lower():
+                output = f"*Company name*: {raw[i]}\n *Stock code*: {i}\n *scripCode*: {raw['securityID']}\n \N{money bag} *Current price*: {cls.bseObj.getQuote(i)['currentValue']}\n \u2b06 *dayHigh*: {cls.bseObj.getQuote(i)['dayHigh']}\n \u2b07 *dayLow*: {cls.bseObj.getQuote(i)['dayLow']}\n \N{Chart with Upwards Trend} *52 week high*: {cls.bseObj.getQuote(i)['52weekHigh']}\n \N{Chart with Downwards Trend} *52 week low*: {cls.bseObj.getQuote(i)['52weekLow']}\n "
+            else:
+                continue
+        return output
+
+    @classmethod
+    def get_all_details_by_scripCode(cls, ScripCode) -> str:
+        if ScripCode in cls.bseObj.getScripCodes():
+            raw = cls.bseObj.getQuote(ScripCode)
+            output = f"*Company name*: {raw['companyName']}\n *Stock code*: {raw['securityID']}\n *scripCode*: {raw['scripCode']}\n \N{money bag} *Current price*: {raw['currentValue']}\n \u2b06 *dayHigh*: {raw['dayHigh']}\n \u2b07 *dayLow*: {raw['dayLow']}\n \N{Chart with Upwards Trend} *52 week high*: {raw['52weekHigh']}\n \N{Chart with Downwards Trend} *52 week low*: {raw['52weekLow']}\n "
+            return output
+        else:
+            return "Invalid Scrip Code"
